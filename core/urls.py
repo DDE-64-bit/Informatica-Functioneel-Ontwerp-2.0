d6ef27df9d751cc3236160c8b39f6cb7752a3d20
DDE <119262202+DDE-64-bit@users.noreply.github.com>
2024-02-29 15:22:13 +0000

    changes (stable)

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('aanmelden', views.aanmelden, name='aanmelden'),
+    path('uitloggen', views.uitloggen, name='uitloggen'),
+    path('post_list', views.post_list, name='post_list'), 
+    path("maak_post", views.maak_post, name="maak_post"),
+]
